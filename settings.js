cb827815c1dcb23d7612261ba318a407e312c1fa
botneyo <112771315+botneyo@users.noreply.github.com>
2022-09-05 05:27:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['96891103640']
-global.premium = ['96891103640']
-global.ownernomer = '96891103640'
+global.owner = ['9470']
+global.premium = ['97640']
+global.ownernomer = '903740'
 global.ownername = '♛ الامبراطور زورو ♛'
 global.botname = '♛ نيو بوت ♛'
 global.footer = ' نيو بوت ♛'
